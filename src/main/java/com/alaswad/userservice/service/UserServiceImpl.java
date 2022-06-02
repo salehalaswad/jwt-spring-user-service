421f09913f235b67f99b4b8b013f0959ee45489f
salehalaswad <s.alaswad@outlook.de>
2022-06-02 19:27:14 +0200

    Spring Security Configuration

--- src/main/java/com/alaswad/userservice/service/UserServiceImpl.java
+++ src/main/java/com/alaswad/userservice/service/UserServiceImpl.java
@@ -6,19 +6,40 @@ import com.alaswad.userservice.repo.RoleRepo;
 import com.alaswad.userservice.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j // for logging
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = userRepo.findByUsername(username);
+        if (user==null){
+            log.error("user not found  in the database");
+            throw new UsernameNotFoundException("User not found");
+        }else {
+            log.info("user found in the database {}",username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+        return new User(user.getUsername(),user.getPassword(),authorities); //from spring package
+    }
 
     @Override
     public AppUser saveUser(AppUser user) {
